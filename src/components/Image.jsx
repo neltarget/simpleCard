b37e06a06af3cc37e2a641b1e46e89766ab7d413
neltarget <mart.phil23@gmail.com>
2025-05-17 05:14:57 +0000

    updated css

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const Image = (props) => {
   return (
-      <img className="image"
-        src={props.imageUrl}
-        width={"150px"}
-        height={"150px"}
-        alt="image-description"
-      />
+    <img
+      className="image"
+      src={props.imageUrl}
+      width={"150px"}
+      height={"150px"}
+      alt="image-description"
+    />
   );
 };
 
